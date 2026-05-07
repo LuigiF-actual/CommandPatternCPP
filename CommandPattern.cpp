7690deee85cf8229611dfb05ca3af29b0ccb9a88
LuigiF <246063605+LuigiF-actual@users.noreply.github.com>
2026-05-07 19:20:30 +0200

    Add project files.

---
+++ CommandPattern.cpp
@@ -0,0 +1,12 @@
+﻿// CommandPattern.cpp : Defines the entry point for the application.
+//
+
+#include "CommandPattern.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
